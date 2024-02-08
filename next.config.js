e468c254e78fd81876ab1823fb33f45c85c26863
BrownieBits <ian.scot.brown@gmail.com>
2024-02-07 18:20:58 -0700

    build: updated images array

--- next.config.js
+++ next.config.js
@@ -26,6 +26,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'amaze-dash.netlify.app',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
