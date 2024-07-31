b7de10cd86757bb0e18e2d787a5b86f24af78c69
Your Name <you@example.com>
2024-07-31 01:48:38 -0600

    fix: updated styling and colors

--- app/(user)/dashboard/analytics/page.tsx
+++ app/(user)/dashboard/analytics/page.tsx
@@ -87,7 +87,6 @@ export default function Analytics() {
               </PopoverTrigger>
               <PopoverContent className="w-auto p-0" align="start">
                 <Calendar
-                  initialFocus
                   mode="range"
                   defaultMonth={date?.from}
                   selected={date}
